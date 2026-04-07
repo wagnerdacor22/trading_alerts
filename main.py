fad985c2567c2e70bfdb1a05ab2b40e4fa57a757
wagnerdacor22 <wagnerdacor@gmail.com>
2026-04-07 17:10:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -108,3 +108,6 @@ if __name__ == '__main__':
     
     print("🚀 Servidor iniciado!")
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+# Deploy forçado em 07/04/2026 - 17:10
